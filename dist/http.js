ec47ef8a4d49ec27cf3bdaf148c02edf67f26e47
wen <1127622940@qq.com>
2015-10-14 13:52:20 +0800

    Update http.js

--- dist/http.js
+++ dist/http.js
@@ -11,7 +11,7 @@ var Conf = function() {
         CACHE: 'no-cache',
         DEFAULT_INDEX: ['index.html', 'index.htm', 'index.asp', 'index.php', 'index.json', 'index.txt', 'index.njs'],//默认首页索引
         EXTEND_EXT: '.njs',//扩展后缀
-        MAX_QUEUE_LENGTH: 2,
+        MAX_QUEUE_LENGTH: 10,
 
         /*允许用于并发处理CGI的最大进程数,为了避免主进程阻塞，CGI使用子进程处理，CGI会进入一个等待队列，等待执行，进程越多，并发处理能力越强，但系统资源占用越多*/
 
